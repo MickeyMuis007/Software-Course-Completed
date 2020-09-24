832b05de4743872a689419ca4775a03a55a36563
Michael <mihendricks1@gmail.com>
2020-09-24 09:13:06 +0200

    working throught .net core s3 course

---
+++ software-courses/Pluralsight/AWS/AWS Dotnet Core Using S3/02/demos/after/LifeBackup/LifeBackup.Infrastructure/Repositories/BucketRepository.cs
@@ -0,0 +1,11 @@
+﻿using LifeBackup.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeBackup.Infrastructure.Repositories
+{
+    public class BucketRepository : IBucketRepository
+    {
+    }
+}
